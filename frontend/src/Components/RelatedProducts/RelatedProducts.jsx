219f22960d8a9465c19f468c54523a83390d4f37
Alyson <arodriguezq3@miumg.edu.gt>
2024-03-26 11:37:17 -0600

    9959-21-829 newsletter, item, relatedproducts, shop (.css .jsx)

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,1 +1,23 @@
-2:18:28 - 2:20:12
+RelatedProducts.jsx
+
+
+import React from 'react'
+import './RelatedProducts.css'
+import Item from '../Item/Item'
+
+const RelatedProducts = () => {
+  return (
+    <div className='relatedproducts'>
+        <h1>Related Products</h1>
+        <hr />
+        <div className="relatedproducts-item">
+            {data_product.map((item,i)=>{
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/> 
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default RelatedProducts
+
